c96a10c59169c2f780a4b234ec63beef988c59a3
xeon_lagunas <xeon.lagunas@gmail.com>
2023-12-20 23:32:09 +0900

    セーブシステム追加

---
+++ Assets/Scripts/Utility/SaveSystem/Compressor.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+public class Compressor
+{
+    public static byte[] Compress(byte[] rawData)
+    {
+        using (var compressedStream = new MemoryStream())
+        {
+            using (var gZipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+                gZipStream.Write(rawData, 0, rawData.Length);
+            return compressedStream.ToArray();
+        }
+    }
+
+    public static byte[] Decompress(byte[] compressedData)
+    {
+        using (var compressedStream = new MemoryStream(compressedData))
+        using (var decompressedStream = new MemoryStream())
+        {
+            using (GZipStream gZipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+                gZipStream.CopyTo(decompressedStream);
+            return decompressedStream.ToArray();
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/SaveSystem/DataBank.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+using System.Text;
+
+public class DataBank
+{
+    private static DataBank instance = new DataBank();
+    private static Dictionary<string, object> bank = new();
+
+    private const string path = "SaveData";
+    private const string extension = "dat";
+    private static readonly string fullPath = $"{Application.persistentDataPath}/{path}";
+
+    public string SavePath => fullPath;
+
+    private DataBank() { }
+
+    public static DataBank Instance => instance;
+
+    public bool IsEmpty() => bank.Count == 0;
+
+    public bool ExistsKey(string key) => bank.ContainsKey(key);
+
+    public void Store(string key, object obj) => bank[key] = obj;
+
+    public void Clear() => bank.Clear();
+
+    public void Remove(string key) => bank.Remove(key);
+
+    public DataType Get<DataType>(string key) => ExistsKey(key) ? (DataType)bank[key] : default;
+
+    public void SaveAll()
+    {
+        foreach (var key in bank.Keys)
+            Save(key);
+    }
+
+    public bool Save(string key)
+    {
+        if (!ExistsKey(key)) return false;
+
+        var filePath = $"{fullPath}/{key}.{extension}";
+
+        var json = JsonUtility.ToJson(bank[key]);
+
+        var data = Encoding.UTF8.GetBytes(json);
+        data = Compressor.Compress(data);
+        data = Cryptor.Encrypt(data);
+
+        if (!Directory.Exists(fullPath))
+            Directory.CreateDirectory(fullPath);
+
+        using (var fileStream = File.Create(filePath))
+            fileStream.Write(data, 0, data.Length);
+
+        return true;
+    }
+
+    public bool Load<DataType>(string key)
+    {
+        var filePath = $"{fullPath}/{key}.{extension}";
+
+        if (!File.Exists(filePath)) return false;
+
+        byte[] data = null;
+        using (var fileStream = File.OpenRead(filePath))
+        {
+            data = new byte[fileStream.Length];
+            fileStream.Read(data, 0, data.Length);
+        }
+
+        data = Cryptor.Decrypt(data);
+        data = Compressor.Decompress(data);
+
+        var json = Encoding.UTF8.GetString(data);
+
+        bank[key] = JsonUtility.FromJson<DataType>(json);
+
+        return true;
+    }
+}
